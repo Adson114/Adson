617a591270fba36cd97c1066f4c2d720ff4a30c6
Adson114 <643501688@qq.com>
2020-03-08 14:17:24 +0800

    spring boot curd 第二次SQL提交

---
+++ curd.sql
@@ -0,0 +1,23 @@
+create database thymeleaf;
+use thymeleaf;
+CREATE TABLE user (
+id int(11) NOT NULL AUTO_INCREMENT,
+name varchar(30),
+PRIMARY KEY (id)
+) DEFAULT CHARSET=UTF8;
+
+INSERT INTO user VALUES (1,'name1');
+INSERT INTO user VALUES (2,'name2');
+INSERT INTO user VALUES (3,'name3');
+INSERT INTO user VALUES (4,'name4');
+INSERT INTO user VALUES (5,'name5');
+INSERT INTO user VALUES (6,'name6');
+INSERT INTO user VALUES (7,'name7');
+INSERT INTO user VALUES (8,'name8');
+INSERT INTO user VALUES (9,'name9');
+INSERT INTO user VALUES (10,'name10');
+INSERT INTO user VALUES (11,'name11');
+INSERT INTO user VALUES (12,'name12');
+INSERT INTO user VALUES (13,'name13');
+INSERT INTO user VALUES (14,'name14');
+INSERT INTO user VALUES (15,'name15');
